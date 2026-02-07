161bfec0706b91136dca75a351cc959b0aa8d7ed
Jinxer <jinxer2426@gmail.com>
2026-02-07 22:06:05 +0530

    Local Swagger Server

---
+++ server.py
@@ -0,0 +1,21 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+import os
+import json
+
+class Handler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "../specs":
+            specs = [
+                f for f in os.listdir(".")
+                if f.endswith((".yaml", ".yml", ".json"))
+            ]
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(specs).encode())
+        else:
+            super().do_GET()
+
+if __name__ == "__main__":
+    HTTPServer(("0.0.0.0", 8080), Handler).serve_forever()
+
